61a7312164e247972a5f072ced2c360745dd384c
dreamCode <czon.herald75@gmail.com>
2024-01-15 19:28:25 +0545

    feat: added dummy egg-animation

--- src/app/egg-me/page.jsx
+++ src/app/egg-me/page.jsx
@@ -1,13 +1,20 @@
 "use client";
 import React, { useState } from "react";
 import Stopwatch from "./stopwatch/StopWatch";
+import eggAnimation from "../../../public/image/egg-animation.gif";
+import Image from "next/image";
 
 const EggMe = () => {
   const [activeEgg, setActiveEgg] = useState("Soft");
 
   return (
     <div className="h-[100vh] flex items-center justify-center">
-      <div>
+      <div className="">
+        <Image
+          className="text-center  w-full"
+          src={eggAnimation}
+          alt="Picture of the egg loading"
+        />
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           onClick={() => setActiveEgg("Soft")}
